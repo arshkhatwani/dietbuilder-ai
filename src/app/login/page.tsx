f7983126374264840be99d79266667da415a0d4f
arshkhatwani <arsh.khatwani@gmail.com>
2024-06-16 18:32:52 +0530

    feat: adds heading in login screen

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,7 +12,16 @@ export default async function LoginPage() {
     }
 
     return (
-        <div className="min-h-screen flex justify-center items-center">
+        <div className="min-h-screen flex flex-col gap-9 justify-center items-center">
+            <div className="text-center">
+                <h2 className="text-3xl font-bold tracking-tight md:text-4xl mb-1">
+                    DietBuilder AI
+                </h2>
+                <p className="text-gray-500 dark:text-gray-400">
+                    Login to get started!
+                </p>
+            </div>
+
             <form action={loginWithGoogle}>
                 <GoogleSignIn />
             </form>
