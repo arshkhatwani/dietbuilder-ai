dae2b7e9839c6991e4064138a49d83f33de3c271
arshkhatwani <arsh.khatwani@gmail.com>
2024-06-16 12:59:11 +0530

    bugfix: add dynamic origin

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -2,16 +2,20 @@
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { headers } from "next/headers";
 
 import { createClient } from "@/lib/supabase/server";
 
 export async function loginWithGoogle() {
     const supabase = createClient();
 
+    const headersList = headers();
+    const origin = headersList.get("origin");
+
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-            redirectTo: process.env.NEXT_PUBLIC_AUTH_CALLBACK_URL,
+            redirectTo: `${origin}/auth/callback`,
             queryParams: {
                 prompt: "consent",
             },
